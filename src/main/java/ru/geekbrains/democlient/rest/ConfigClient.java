d0a84af733b72e15650f427b6f5bf976d439b144
Artem Kropotov <AAKropotov@alfabank.ru>
2021-03-14 19:49:01 +0300

    Initial config client

---
+++ src/main/java/ru/geekbrains/democlient/rest/ConfigClient.java
@@ -0,0 +1,25 @@
+package ru.geekbrains.democlient.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+
+@RestController
+public class ConfigClient {
+
+    @Value("{user.role}")
+    private String role;
+
+    @GetMapping(
+            value = "/whoami/{username}",
+            produces = TEXT_PLAIN_VALUE)
+    public String whoAmI(@PathVariable("username") String username) {
+        return String.format("Hello! You're %s and you'll become a(n) %s...",
+                username, role);
+    }
+}
---
+++ src/test/java/ru/geekbrains/democlient/DemoClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.democlient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
